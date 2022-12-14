c71660fefd0a181fb1f494377a7b31f38efd2187
Ikikaeru <39247240+Ikikaeru@users.noreply.github.com>
2022-12-15 00:09:35 +0100

    Cron

--- lazy-mods/cron/registerCRON.js
+++ lazy-mods/cron/registerCRON.js
@@ -1,5 +1,6 @@
 const { platform } = require('node:process');
 const { spawn } = require('child_process');
+const os = require('os');
 // const registerCRONTask_WINDOWS = (taskName) => {
 //     return [
 //         "$action = New-ScheduledTaskAction -Execute 'cmd.exe'",
@@ -35,23 +36,53 @@ module.exports = (program, config) => {
         .option('-d, --daily <dailys...>', 'Create a daily CRON command with the time specified as: <dailys...> := <hour> [min]')
         .action((taskName, command, options) => {
             if(options.daily) {
-                let h, m;
+                let h, m, s;
                 if(options.daily) {
-                    if(options.daily.length > 1) {
-                        h = options.daily[0];
-                        m = options.daily[1];
-                    } else {
-                        h = options.daily[0];
-                        m = 0;
+                    switch(options.daily.length) {
+                        case 1:
+                            h = options.daily[0];
+                            m = 0;
+                            s = 0;
+                            break;
+                        case 2:
+                            h = options.daily[0];
+                            m = options.daily[1];
+                            s = 0;
+                            break;
+                        case 3:
+                            h = options.daily[0];
+                            m = options.daily[1];
+                            s = options.daily[2];
+                            break;
+                        default:
+                            h = 0;
+                            m = 0;
+                            s = 0;
+                            break;
                     }
                 }
                 const hours = h % 24;
                 const min = m % 60;
-                const commands = [
-                    `schtasks /create /tn "${taskName}" /tr "${command}" /sc daily /st ${hours < 10 ? `0${hours}` : hours}:${min < 10 ? `0${min}` : min}`
-                ];
+                const sec = s % 60;
+                const time = `${hours < 10 ? `0${hours}` : hours}:${min < 10 ? `0${min}` : min}:${sec < 10 ? `0${sec}` : sec}`;
 
-                const newShell = spawn('powershell', ['-command', ...commands]);
+                if (os.platform() === 'win32') {
+                    // Schedule a task to run every day at 7pm using schtasks on Windows
+                    const newShell = spawn('powershell', ['/create', '/tn', taskName, '/tr', command, '/sc', 'DAILY', '/st', '19:00:00']);
+                    newShell.on('exit', () => console.log(`The CRON ${taskName} has been set to ${time}.`));
+                } else {
+                    // Open the crontab editor in read-only mode, so the user cannot edit it
+                    const crontabEditor = spawn('crontab', ['-l'], { stdio: 'inherit' });
+                    crontabEditor.on('exit', () => {
+                        console.log(`The CRON ${taskName} has been set to ${time}.`)
+                    });
+                    // Inject the new line with the schedule information and the command to run
+                    // ts-node app.ts every day at 7pm
+                    crontabEditor.stdin.write('0 19 * * * ts-node app.ts\n');
+                    // Close the crontab editor
+                    crontabEditor.stdin.end();
+                }
+                
                 console.log(commands);
                 //TaskCRON.daily();
             }
